936edb6f4ca4a6df99f01f0ee04ee4cfa4d9dd71
Xon <635541+Xon@users.noreply.github.com>
2023-11-07 18:37:51 +0800

    cleanup

--- upload/src/addons/SV/EmailQueue/_output/extension_hint_fake.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** @noinspection PhpMultipleClassesDeclarationsInOneFile */
-/** @noinspection PhpIllegalPsrClassPathInspection */
-
-// XF2.1 helper defines
-class Swift_Mime_Message {}
-class FinalSwiftMimeMessage extends \Swift_Mime_SimpleMessage {}
