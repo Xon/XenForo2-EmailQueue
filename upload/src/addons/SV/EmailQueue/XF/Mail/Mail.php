35c99d751907e2ee5c847fc7c23b5eca53c923d7
Xon <635541+Xon@users.noreply.github.com>
2023-11-07 18:24:00 +0800

    Cleanup overriding email sending

--- upload/src/addons/SV/EmailQueue/XF/Mail/Mail.php
+++ upload/src/addons/SV/EmailQueue/XF/Mail/Mail.php
@@ -2,61 +2,30 @@
 
 namespace SV\EmailQueue\XF\Mail;
 
-/**
- * @property Mailer $mailer
- */
+use function array_fill_keys;
+use function array_key_exists;
+
 class Mail extends XFCP_Mail
 {
-    /** @var array|null */
+    /** @var array<string,true>|null */
     protected $svEmailQueueExclude = null;
-
-    /** @noinspection PhpMissingReturnTypeInspection */
-    /**
-     * @param string $subject
-     * @param string $htmlBody
-     * @param string|null $textBody
-     * @return Mail
-     */
-    public function setContent($subject, $htmlBody, $textBody = null)
-    {
-        if (\XF::$versionId < 2020070)
-        {
-            $subject = (string)$subject;
-            $subject = \preg_replace('#[\r\n\t]\s*#', ' ', $subject);
-            $subject = \preg_replace('#( ){2,}#', ' ', $subject);
-            $subject = \trim($subject);
-
-            // ensure body is cast to string to avoid trying to send phrases as content
-            $htmlBody = (string)$htmlBody;
-            $textBody = (string)$textBody;
-            if (!\strlen($textBody))
-            {
-                $textBody = null;
-            }
-        }
-
-        return parent::setContent($subject, $htmlBody, $textBody);
-    }
+    /** @var bool */
+    protected $svForceQueue = false;
 
     public function send(\Swift_Transport $transport = null, $allowRetry = true)
     {
-        if ($this->setupError)
-        {
-            $this->logSetupError($this->setupError);
-            return 0;
-        }
-
-        $options = \XF::options();
         if ($this->svEmailQueueExclude === null)
         {
-            $this->svEmailQueueExclude = \array_fill_keys($options->sv_emailqueue_exclude, true);
+            $options = \XF::options();
+            $this->svForceQueue = $options->sv_emailqueue_force ?? false;
+            $this->svEmailQueueExclude = array_fill_keys(\XF::options()->sv_emailqueue_exclude ?? [], true);
         }
 
-        if ($options->sv_emailqueue_force && empty($this->svEmailQueueExclude[$this->templateName]))
+        if ($this->svForceQueue && !array_key_exists($this->templateName, $this->svEmailQueueExclude))
         {
             return $this->queue();
         }
 
-        return parent::send($transport, false);
+        return parent::send($transport, $allowRetry);
     }
 }
