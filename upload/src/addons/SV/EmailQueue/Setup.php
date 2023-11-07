420224a5b68c3b0b768d720e4ddd92fb54d7828b
Xon <635541+Xon@users.noreply.github.com>
2023-11-07 18:43:39 +0800

    Simplify installer

--- upload/src/addons/SV/EmailQueue/Setup.php
+++ upload/src/addons/SV/EmailQueue/Setup.php
@@ -15,37 +15,13 @@ class Setup extends AbstractSetup
     use StepRunnerUpgradeTrait;
     use StepRunnerUninstallTrait;
 
-    public function upgrade2000000Step1()
+    public function upgrade2030000Step1(): void
     {
-        $this->cleanupOldTable(false);
+        $this->db()->query('DROP TABLE IF EXISTS xf_mail_queue_failed');
     }
 
-    public function upgrade2030000Step1()
+    public function uninstallStep1(): void
     {
-        $this->cleanupOldTable();
-    }
-
-    public function upgrade2030000Step2()
-    {
-        $this->renameOption('sv_emailqueue_failures_to_error','svEmailQueue_retryToAbandon');
-    }
-
-    public function uninstallStep1()
-    {
-        $this->cleanupOldTable();
-    }
-
-    protected function cleanupOldTable(bool $requeueContents = true)
-    {
-        if ($requeueContents)
-        {
-            $this->db()->query('
-                INSERT INTO xf_mail_queue (`mail_data`,`queue_date`)
-                SELECT `mail_data`,`queue_date`
-                FROM xf_mail_queue_failed
-                WHERE dispatched = 0
-            ');
-        }
         $this->db()->query('DROP TABLE IF EXISTS xf_mail_queue_failed');
     }
 }
