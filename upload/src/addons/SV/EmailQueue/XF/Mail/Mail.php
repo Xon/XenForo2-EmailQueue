59e8ce271236e5497ab3ea75aa4844d3ad552ff9
Xon <635541+Xon@users.noreply.github.com>
2025-02-20 03:02:59 +0800

    XF2.3 support

--- upload/src/addons/SV/EmailQueue/XF/Mail/Mail.php
+++ upload/src/addons/SV/EmailQueue/XF/Mail/Mail.php
@@ -2,34 +2,12 @@
 
 namespace SV\EmailQueue\XF\Mail;
 
-use function array_fill_keys;
-use function array_key_exists;
+use SV\StandardLib\Helper;
+use SV\EmailQueue\XF\Mail\XF22\MailPatch as MailPatchXF22;
 
-/**
- * XF2.2
- * @extends \XF\Mail\Mail
- */
-class Mail extends XFCP_Mail
-{
-    /** @var array<string,true>|null */
-    protected $svEmailQueueExclude = null;
-    /** @var bool */
-    protected $svForceQueue = false;
-
-    public function send(\Swift_Transport $transport = null, $allowRetry = true)
-    {
-        if ($this->svEmailQueueExclude === null)
-        {
-            $options = \XF::options();
-            $this->svForceQueue = $options->sv_emailqueue_force ?? false;
-            $this->svEmailQueueExclude = array_fill_keys($options->sv_emailqueue_exclude ?? [], true);
-        }
-
-        if ($this->svForceQueue && !array_key_exists($this->templateName, $this->svEmailQueueExclude))
-        {
-            return $this->queue();
-        }
-
-        return parent::send($transport, $allowRetry);
-    }
-}
+Helper::repo()->aliasClass(
+    'SV\EmailQueue\XF\Mail',
+    \XF::$versionId < 2030000
+        ? MailPatchXF22::class
+        : MailPatch::class
+);
---
+++ upload/src/addons/SV/EmailQueue/XF/Mail/MailPatch.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SV\EmailQueue\XF\Mail;
+
+use Symfony\Component\Mailer\Transport\AbstractTransport;
+use XF\Mail\Mail;
+use function array_fill_keys;
+use function array_key_exists;
+
+/**
+ * XF2.3+
+ * @extends Mail
+ */
+class MailPatch extends XFCP_Mail
+{
+    /** @var array<string,true>|null */
+    protected $svEmailQueueExclude = null;
+    /** @var bool */
+    protected $svForceQueue = false;
+
+    public function send(?AbstractTransport $transport = null, $allowRetry = true)
+    {
+        if ($this->svEmailQueueExclude === null)
+        {
+            $options = \XF::options();
+            $this->svForceQueue = $options->sv_emailqueue_force ?? false;
+            $this->svEmailQueueExclude = array_fill_keys($options->sv_emailqueue_exclude ?? [], true);
+        }
+
+        if ($this->svForceQueue && !array_key_exists($this->templateName, $this->svEmailQueueExclude))
+        {
+            return $this->queue();
+        }
+
+        return parent::send($transport, $allowRetry);
+    }
+}
