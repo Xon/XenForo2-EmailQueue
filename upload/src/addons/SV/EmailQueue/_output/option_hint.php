4fdb1d15c149b3afa35fef86a40036360a179de0
Xon <635541+Xon@users.noreply.github.com>
2025-02-20 02:54:27 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/EmailQueue/_output/option_hint.php
@@ -0,0 +1,20 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property array{two_step_login_backup: int, two_step_login_email: int, user_email_confirmation: int, user_lost_password: int, user_lost_password_reset: int}|null $sv_emailqueue_exclude Email templates to exclude from queueing
+ * @property non-negative-int|null $sv_emailqueue_failures_to_error Failed email abandon threshold
+ * @property bool|null $sv_emailqueue_force Queue all email
+ */
+class Options
+{
+}
